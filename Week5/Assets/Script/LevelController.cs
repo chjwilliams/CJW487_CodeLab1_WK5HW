dc412bf5773144a370d8fb1d40e42916a59789f7
chjwilliams <chrsjwilliams@gmail.com>
2017-02-26 21:40:42 -0500

    Basic Interaction Implemented
    Scroll to move the level. Get the sphere into the cube. Win state not yet programmed.

---
+++ Week5/Assets/Script/LevelController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour 
+{
+
+	public float rotationSpeed = 2.0f;
+	private float zRotation;
+	private GameObject level;
+
+	public const string SCROLLWHEEL = "Mouse ScrollWheel";
+	public const string LEVEL_HOLDER = "Level Holder";
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	void RotateLevel(float z)
+	{
+		level.transform.localRotation = Quaternion.Euler(level.transform.rotation.x, level.transform.rotation.y, z * rotationSpeed);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (level == null)
+		{
+			level = GameObject.Find(LEVEL_HOLDER);
+		}
+
+		zRotation = zRotation + Input.GetAxis(SCROLLWHEEL);
+		RotateLevel(zRotation);
+	}
+}
---
+++ Week5/Assets/Script/LevelLoader.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour 
+{
+
+	public float offsetX = -6;
+	public float offsetY = 8;
+
+	public string[] fileNames;
+	public const char KEY_WALL = '*';
+	public const char KEY_PLAYER = 'P';
+	public const char KEY_GOAL = 'G';
+	public const string THIS_SCENE = "Week5";
+	public const string LEVEL_PATH = "/Resources/Levels/";
+	public const string FILE_TYPE = ".txt";
+	public const string LEVEL_HOLDER = "Level Holder";
+	
+	public static int levelNum = 0;
+
+	private bool playerHasBeenLoaded;
+	private bool goalHasBeenLoaded;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerHasBeenLoaded = false;
+		string fileName = fileNames[levelNum];
+
+		string filePath = Application.dataPath + LEVEL_PATH + fileName + FILE_TYPE;
+
+		StreamReader sr = new StreamReader(filePath);
+
+		GameObject levelHolder = new GameObject(LEVEL_HOLDER);
+
+		int yPos = 0;
+
+		
+
+		while(!sr.EndOfStream)
+		{
+			string line = sr.ReadLine();
+
+			for(int xPos = 0; xPos < line.Length; xPos++)
+			{
+
+				if(line[xPos] == KEY_WALL)
+				{
+					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+					cube.transform.parent = levelHolder.transform;
+
+					cube.transform.position = new Vector3( xPos + offsetX, yPos + offsetY, 0);
+					cube.AddComponent<Rigidbody>();
+					cube.GetComponent<Rigidbody>().useGravity = false;
+					cube.GetComponent<Rigidbody>().isKinematic = true;
+				}
+
+				if(line[xPos] == KEY_PLAYER && !playerHasBeenLoaded)
+				{
+					GameObject player = Instantiate(Resources.Load("Prefabs/Player") as GameObject);
+					player.transform.position = new Vector3 (xPos + offsetX, yPos + offsetY, 0);
+					playerHasBeenLoaded = true;
+				}
+
+				if(line[xPos] == KEY_GOAL && !goalHasBeenLoaded)
+				{
+					GameObject goal = Instantiate(Resources.Load("Prefabs/Goal") as GameObject);
+					goal.transform.position = new Vector3 (xPos + offsetX, yPos + offsetY, 0);
+					goal.transform.parent = levelHolder.transform;
+					goalHasBeenLoaded = true;
+				}
+			}
+			yPos--;
+		}
+
+		sr.Close();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			levelNum++;
+			SceneManager.LoadScene(THIS_SCENE);
+		}
+	}
+}
