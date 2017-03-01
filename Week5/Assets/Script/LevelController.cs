2585fb19afb825a13d9359f30a7c30b27ffa8d84
chjwilliams <chrsjwilliams@gmail.com>
2017-03-01 02:26:30 -0500

    Scroll to play. Second level added. Scripts commented

--- Week5/Assets/Script/LevelController.cs
+++ Week5/Assets/Script/LevelController.cs
@@ -2,35 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*--------------------------------------------------------------------------------------*/
+/*																						*/
+/*	LevelController: rotates level based on scroll input								*/
+/*			Functions:																	*/
+/*					public:																*/
+/*																					    */
+/*					proteceted:															*/
+/*                                                                                      */
+/*					private:															*/
+/*						void RotateLevel (float z)										*/
+/*						void Uppdate ()													*/
+/*																						*/
+/*--------------------------------------------------------------------------------------*/
 public class LevelController : MonoBehaviour 
 {
+	//	Public Variables
+	public float rotationSpeed = 2.0f;							//	How fast the level rotates
+	public const string SCROLLWHEEL = "Mouse ScrollWheel";		//	Name reference to the scroll wheel axis
+	public const string LEVEL_HOLDER = "Level Holder";			//	Name reference to Level Holder
 
-	public float rotationSpeed = 2.0f;
-	private float zRotation;
-	private GameObject level;
-
-	public const string SCROLLWHEEL = "Mouse ScrollWheel";
-	public const string LEVEL_HOLDER = "Level Holder";
-	// Use this for initialization
-	void Start () 
-	{
-		
-	}
+	//	Private Variables
+	private float _ZRotation;									//	Stores the scroll input value
+	private GameObject _Level;									//	Reference to Level Holder
 	
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	RotateLevel: Rotates the enitre level												*/
+    /*		param: float z - the value taken from the scroll wheel input					*/
+	/*																						*/
+    /*--------------------------------------------------------------------------------------*/
 	void RotateLevel(float z)
 	{
-		level.transform.localRotation = Quaternion.Euler(level.transform.rotation.x, level.transform.rotation.y, z * rotationSpeed);
+		//	Where the rotation magic happens
+		_Level.transform.localRotation = Quaternion.Euler(_Level.transform.rotation.x, _Level.transform.rotation.y, z * rotationSpeed);
 	}
 
-	// Update is called once per frame
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	Update: Update is called once per frame												*/
+    /*																						*/
+    /*--------------------------------------------------------------------------------------*/
 	void Update () 
 	{
-		if (level == null)
+		//	If level is null set level to Level Holder
+		if (_Level == null)
 		{
-			level = GameObject.Find(LEVEL_HOLDER);
+			//	This is line is run once at the begining of every new level
+			_Level = GameObject.Find(LEVEL_HOLDER);
 		}
 
-		zRotation = zRotation + Input.GetAxis(SCROLLWHEEL);
-		RotateLevel(zRotation);
+		//	Gets the value from the scroll wheel input
+		_ZRotation = _ZRotation + Input.GetAxis(SCROLLWHEEL);
+		//	Passes _ZRotation into the RotateLevel function
+		RotateLevel(_ZRotation);
 	}
 }
--- Week5/Assets/Script/LevelLoader.cs
+++ Week5/Assets/Script/LevelLoader.cs
@@ -4,89 +4,165 @@ using UnityEngine;
 using System.IO;
 using UnityEngine.SceneManagement;
 
+/*--------------------------------------------------------------------------------------*/
+/*																						*/
+/*	LevelLoader: Loads the levels														*/
+/*			Functions:																	*/
+/*					public:																*/
+/*						void ReloadLevel ()											    */
+/*						void LoadNewLevel ()										    */
+/*																					    */
+/*					proteceted:															*/
+/*                                                                                      */
+/*					private:															*/
+/*						void Start ()													*/
+/*																						*/
+/*--------------------------------------------------------------------------------------*/
 public class LevelLoader : MonoBehaviour 
 {
-
-	public float offsetX = -6;
-	public float offsetY = 8;
-
-	public string[] fileNames;
-	public const char KEY_WALL = '*';
-	public const char KEY_PLAYER = 'P';
-	public const char KEY_GOAL = 'G';
-	public const string THIS_SCENE = "Week5";
-	public const string LEVEL_PATH = "/Resources/Levels/";
-	public const string FILE_TYPE = ".txt";
-	public const string LEVEL_HOLDER = "Level Holder";
+	//	Public Static Variables
+	public static int levelNum = 0;									//	The current level number
+	public static LevelLoader instance;								//	Instance of Level Loader
 	
-	public static int levelNum = 0;
+	//	Public Constant Variables
+	public const char KEY_WALL = '*';								//	The character in the txt file that means wall
+	public const char KEY_PLAYER = 'P';								//	The character in the txt file that means player
+	public const char KEY_GOAL = 'G';								//	The character in the txt file that means goal
+	public const string THIS_SCENE = "Week5";						//	Name reference to this scene
+	public const string LEVEL_PATH = "/Resources/Levels/";			//	The path to get to the level documents
+	public const string FILE_TYPE = ".txt";							//	The file type of the levels
+	public const string LEVEL_HOLDER = "Level Holder";				//	The name of the Game Object that holds the walls
+	public const string PLAYER_PREFAB_PATH = "Prefabs/Player";		//	Path to get to the player prefab
+	public const string GOAL_PREFAB_PATH = "Prefabs/Goal";			//	Path to get to the goal prefab
 
-	private bool playerHasBeenLoaded;
-	private bool goalHasBeenLoaded;
+	//	Public Variables
+	public float offsetX = -6;										//	Offset to center the level in x position
+	public float offsetY = 8;										//	Offset to center the level in y position
+	public string[] fileNames;										//	Array of file names. **EDIT IN SINSPECTOR**
+	
+	//	Private Variables
+	private bool playerHasBeenLoaded;								//	Logic gate so only one player is loaded
+	private bool goalHasBeenLoaded;									//	Logic gate so only one goal is loaded
 
-	// Use this for initialization
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	Start: Runs once at the begining of the game. Initalizes variables.					*/
+    /*																						*/
+    /*--------------------------------------------------------------------------------------*/
 	void Start () 
 	{
+		//	Singleton for LevelLoader
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+
+		//	Logic check to make sure only one player is created per level
 		playerHasBeenLoaded = false;
+		
+		//	Sets up file name. Write file name in the Inspector
 		string fileName = fileNames[levelNum];
 
+		//	Sets up file path to find the levels
 		string filePath = Application.dataPath + LEVEL_PATH + fileName + FILE_TYPE;
 
+		//	Creates stream reader
 		StreamReader sr = new StreamReader(filePath);
 
+		//	creates a game object to hold the level
 		GameObject levelHolder = new GameObject(LEVEL_HOLDER);
 
+		//	The value used to exit the while loop
 		int yPos = 0;
 
-		
-
+		//	Read lines until the end of the stream
 		while(!sr.EndOfStream)
 		{
+			//	Reads the line
 			string line = sr.ReadLine();
 
+			//	Reads each character on the line
 			for(int xPos = 0; xPos < line.Length; xPos++)
 			{
-
+				//	Creates a cube where there is a '*'
 				if(line[xPos] == KEY_WALL)
 				{
+					//	Creates a primitive cube for the wall
 					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
+					//	Parents newly created cube to Level Holder
 					cube.transform.parent = levelHolder.transform;
-
+					
+					//	Adjusts position based on offset
 					cube.transform.position = new Vector3( xPos + offsetX, yPos + offsetY, 0);
+
+					//	Adds Rigidbody to cube
 					cube.AddComponent<Rigidbody>();
+					//	Disables gravity for cube
 					cube.GetComponent<Rigidbody>().useGravity = false;
+					//	Sets cube to Kinematic
 					cube.GetComponent<Rigidbody>().isKinematic = true;
 				}
 
+				//	Cretas one player on the first instance of 'P'
 				if(line[xPos] == KEY_PLAYER && !playerHasBeenLoaded)
 				{
-					GameObject player = Instantiate(Resources.Load("Prefabs/Player") as GameObject);
+					//	Creates player based on Player Prefab
+					GameObject player = Instantiate(Resources.Load(PLAYER_PREFAB_PATH) as GameObject);
+					//	Adjusts position based on offset
 					player.transform.position = new Vector3 (xPos + offsetX, yPos + offsetY, 0);
+
+					//	Now another player cannot be created
 					playerHasBeenLoaded = true;
 				}
 
+				//	Creates one goal on the first instance of 'G'
 				if(line[xPos] == KEY_GOAL && !goalHasBeenLoaded)
 				{
-					GameObject goal = Instantiate(Resources.Load("Prefabs/Goal") as GameObject);
+					//	Creates a goal based on Goal Prefab
+					GameObject goal = Instantiate(Resources.Load(GOAL_PREFAB_PATH) as GameObject);
+					//	Adjusts position based on offset
 					goal.transform.position = new Vector3 (xPos + offsetX, yPos + offsetY, 0);
+					//	Parents Goal to level holder so it rotates with the level
 					goal.transform.parent = levelHolder.transform;
+			
+					//	Now another goal cannot be created
 					goalHasBeenLoaded = true;
 				}
 			}
+			// decrements yPos so the while loop will eventually end
 			yPos--;
 		}
-
+		//	Closes stream reader
 		sr.Close();
 	}
-	
-	// Update is called once per frame
-	void Update () 
+
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	ReloadLevel: Reloads the current level												*/
+    /*																						*/
+    /*--------------------------------------------------------------------------------------*/
+	public void ReloadLevel()
 	{
-		if(Input.GetKeyDown(KeyCode.P))
-		{
-			levelNum++;
-			SceneManager.LoadScene(THIS_SCENE);
-		}
+		//	Loads this scene with the same level
+		SceneManager.LoadScene(THIS_SCENE);
+	}
+
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	LoadNewLevel: Loads a new level														*/
+    /*																						*/
+    /*--------------------------------------------------------------------------------------*/
+	public void LoadNewLevel()
+	{
+		//	Adds 1 to level than mods it by 2. The values gotten here are 0 and 1
+		levelNum = (levelNum + 1) % 2;
+		//	Loads this scene with a new level based on levelNum
+		SceneManager.LoadScene(THIS_SCENE);
 	}
 }
